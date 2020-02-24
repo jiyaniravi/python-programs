46fb5a54a120e2872ae91afe10eaced5f4af29a8
Ravi Jiyani <jiyaniravi@gmail.com>
2020-02-24 11:23:53 +0530

    Exception handling demos added

--- python-programs/Basics/MyPythonPackage/ExceptionHandlingDemos/try_except_finally_else.py
+++ python-programs/Basics/MyPythonPackage/ExceptionHandlingDemos/try_except_finally_else.py
@@ -7,7 +7,7 @@ else:
 finally:
     print('Finally always gets called !')
 
-#####################################################
+print('#####################################################')
 
 try:
     number = 10+11
@@ -18,4 +18,36 @@ else:
 finally:
     print('Finally always gets called !')
 
-#####################################################
+print('#####################################################')
+
+try:
+    f = open('test.txt','r')
+    f.write('This is simple text !')
+except TypeError:
+    print('Type error got caught !')
+except OSError:
+    print('OS error got caught !')
+else:
+    print('Else got caught')
+finally:
+    print('I will get executed !')
+
+print('#####################################################')
+
+def ask_for_int():
+    while True:
+        try:
+            number = int(input('Please enter number : '))
+        except:
+            print('Entered value is in correct !')
+            continue
+        else:
+            print('Thanks for providing correct value !')
+            break
+        finally:
+            print('End of function')
+
+ask_for_int()
+
+print('#####################################################')
+
