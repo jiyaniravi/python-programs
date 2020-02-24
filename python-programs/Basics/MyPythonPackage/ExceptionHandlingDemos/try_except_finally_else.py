555951b94ab58073d7b96e2fc71d7595d803ad3b
Ravi Jiyani <jiyaniravi@gmail.com>
2020-02-24 10:21:43 +0530

    Exception handling demo created

---
+++ python-programs/Basics/MyPythonPackage/ExceptionHandlingDemos/try_except_finally_else.py
@@ -0,0 +1,21 @@
+try:
+    number = 10+'10'
+except:
+    print('Exception occured !')
+else:
+    print('Else part got called !')
+finally:
+    print('Finally always gets called !')
+
+#####################################################
+
+try:
+    number = 10+11
+except:
+    print('Exception occured !')
+else:
+    print('Else part got called !')
+finally:
+    print('Finally always gets called !')
+
+#####################################################
