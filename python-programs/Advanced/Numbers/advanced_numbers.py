b6e057bf0e89d10b87d2b3bfa9259c739970f57d
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-03 01:53:59 +0530

    Advanced numbers APIs demos added.

---
+++ python-programs/Advanced/Numbers/advanced_numbers.py
@@ -0,0 +1,14 @@
+import math
+
+print(bin(10))
+print(oct(10))
+print(hex(10))
+
+print(pow(2,3))     #-> 8
+print(pow(2,3,5))   #-> 3
+
+print(abs(-12))     #-> 12
+print(round(3.1))   #-> 3
+print(round(3.9))   #-> 4
+print(round(math.pi, 2))   #-> 4
+
