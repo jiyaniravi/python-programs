9b0b235e44d204d08ae146d56d58b75c7dc45006
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-03 11:20:07 +0530

    String functions demo added

---
+++ python-programs/Advanced/Strings/string_demos.py
@@ -0,0 +1,24 @@
+my_string = 'hello World'
+
+print(my_string.capitalize())
+print(my_string.title())
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.count('o'))
+print(my_string.find('o'))
+print(my_string.center(20,'-'))
+
+s = 'hello 123'
+print(s.isalnum())
+print(s.isalpha())
+
+print(s.islower())
+print(s.isupper())
+print(s.isspace())
+
+print(s.startswith('hello'))
+print(s.endswith('123'))
+
+string = 'hisdhiadhaidh'
+print(string.split('i'))
+print(string.partition('i'))
