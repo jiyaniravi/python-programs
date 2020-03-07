f7b321547613b7f00c296c2db726645a392728b8
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-08 00:25:50 +0530

    Phone number regex added

---
+++ python-programs/Basics/Regex/phone_number.py
@@ -0,0 +1,18 @@
+import re
+
+def isPhoneNumber(number):
+    pattern = re.compile(r'\d\d\d-\d\d\d-\d\d\d',)
+    match = pattern.search(number)
+    print(match.group())
+
+def searchPhoneNumbers(message):
+    pattern = re.compile(r'\d\d\d-\d\d\d-\d\d\d',)
+    return pattern.findall(message)
+
+isPhoneNumber('123-115-1151')
+list_phoneNumbers = searchPhoneNumbers('''This is message ! This is my first phone number : 123-456-7894 
+If I am not available on this number, please call me on my home number : 789-456-1235
+''')
+
+print(list_phoneNumbers)
+print(len(list_phoneNumbers))
