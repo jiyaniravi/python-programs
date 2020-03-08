a85e27dbccbb8039de13a34cc14bf5df2660f614
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-08 17:44:49 +0530

    bat program regex modified

--- python-programs/Basics/Regex/bat_regex.py
+++ python-programs/Basics/Regex/bat_regex.py
@@ -1,8 +1,10 @@
 import re
 
-pattern = re.compile(r'bat(wo)?man')
-bat_list = pattern.search('batman is there but not batwoman')
+pattern = re.compile(r'bat(?:wo)?man')
 bat_list_all = pattern.findall('batman is there but not batwoman')
+batman_match = pattern.search('batman is there')
+batwoman_match = pattern.search('batwoman is there')
 
-print(bat_list.group())
+print(batman_match.group())
+print(batwoman_match.group())
 print(bat_list_all)
