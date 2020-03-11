76d4cc2aae7d3dd2bb8fd4e889d39d9ad7229fc0
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-11 23:59:42 +0530

    shutil and send2trash module demos added

---
+++ python-programs/Basics/Files/remove_files.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+import send2trash
+
+#os.unlink(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3\New')
+#os.removedirs(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
+#shutil.rmtree(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
+
+send2trash.send2trash(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
--- python-programs/Basics/Files/shutil_demos.py
+++ python-programs/Basics/Files/shutil_demos.py
@@ -4,4 +4,4 @@ shutil.copy(r'D:\Knowledge\Python\git\python-programs\Basics\Files\hello.txt',r'
 
 shutil.copytree(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder1',r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
 
-shutil.move(r'D:\Knowledge\Python\git\python-programs\Basics\Files\hello.txt', r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3\New')
+shutil.move(r'D:\Knowledge\Python\git\python-programs\Basics\Files\hello.txt', r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
