4c3a814e985259d45ce7d2ba3c6e8a60fee3cad6
Ravi Jiyani <jiyaniravi@gmail.com>
2020-03-12 05:58:06 +0530

    Deleting files demos added

--- python-programs/Basics/Files/remove_files.py
+++ python-programs/Basics/Files/remove_files.py
@@ -7,3 +7,8 @@ import send2trash
 #shutil.rmtree(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
 
 send2trash.send2trash(r'D:\Knowledge\Python\git\python-programs\Basics\Files\TestFolder3')
+
+for folder_name, sub_folders, file_names in os.walk(r'D:\Knowledge\Python\git\python-programs\Basics\Files'):
+    print('Folder Name : '+folder_name)
+    print('Sub Folders : '+str(sub_folders))
+    print('Files : '+str(file_names))
